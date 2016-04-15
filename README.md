c07d7fdef5411a1a154706ebad3f72c6b176c373
Kevin De Asis <deasis@ualberta.ca>
2016-04-15 02:24:01 -0600

    ¯\_(ツ)_/¯

---
+++ README.md
@@ -0,0 +1,29 @@
+# Messenger Bot Example Using Send/Recieve API
+
+This is a simple Facebook Messenger Bot with Node.js using [Express](http://expressjs.com/)
+
+#Quickstart
+
+In index.js in line 17 and 18. Please insert your token
+
+Follow the steps in [Facebook Messnger Documentation](https://developers.facebook.com/docs/messenger-platform/quickstart)
+
+#Whats in this example
+
+Receive Messages
+
+Sends a Text Message
+
+Sends a Structured Message
+
+Handles Postbacks
+
+# Heroku
+
+A barebones Node.js app using [Express 4](http://expressjs.com/).
+
+This application supports the [Getting Started with Node on Heroku](https://devcenter.heroku.com/articles/getting-started-with-nodejs) article - check it out.
+
+
+
+
---
+++ index.js
@@ -0,0 +1,150 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var request = require('request');
+
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+
+
+app.use(bodyParser.urlencoded({extended: false}))
+// parse application/json
+app.use(bodyParser.json())
+
+
+var token = "INSERT TOKEN HERE"
+var validationToken = "INSERT VERIFY TOKEN HERE"
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function (request, response) {
+    response.render('pages/index');
+});
+
+
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.verify_token'] === validationToken) {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.send('Error, wrong validation token');
+    }
+});
+
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging;
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i];
+        sender = event.sender.id;
+        if (event.message && event.message.text) {
+            text = event.message.text;
+
+            if (text === 'Generic') {
+                sendGenericMessage(sender);
+                continue;
+            }
+
+            console.log(text)
+            sendTextMessage(sender, "Echo: " + text.substring(0, 200));
+
+        }
+        if (event.postback) {
+            text = JSON.stringify(event.postback);
+            sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token);
+            continue;
+        }
+    }
+
+    res.sendStatus(200);
+});
+
+function sendGenericMessage(sender) {
+    messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [
+                    {
+                        "title": "First card",
+                        "subtitle": "Element #1 of an hscroll",
+                        "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                        "buttons": [{
+                            "type": "web_url",
+                            "url": "https://www.messenger.com/",
+                            "title": "Web url"
+                        }, {
+                            "type": "postback",
+                            "title": "Postback",
+                            "payload": "Payload for first element in a generic bubble",
+                        }],
+                    }
+                    , {
+                        "title": "Second card",
+                        "subtitle": "Element #2 of an hscroll",
+                        "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                        "buttons": [{
+                            "type": "postback",
+                            "title": "Postback",
+                            "payload": "Payload for second element in a generic bubble",
+                        }],
+                    }
+                ]
+            }
+        }
+    };
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: token},
+        method: 'POST',
+        json: {
+            recipient: {id: sender},
+            message: messageData,
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+}
+
+
+function sendTextMessage(sender, text) {
+
+    var messageData = {
+        text: text
+    };
+
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: token},
+        method: 'POST',
+        json: {
+            recipient: {id: sender},
+            message: messageData
+        }
+    }, function (error, response) {
+
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+
+    });
+
+}
+
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
+
+
